b5786f9f1a23db105b96b0354e8e13b68b96d19e
Junaidptha <khan619junaid@gmail.com>
2026-03-01 20:07:28 +0530

    Time: 78 ms (45.69%) | Memory: 43.4 MB (21.48%) - LeetSync

--- 1331-path-with-maximum-gold/path-with-maximum-gold.java
+++ 1331-path-with-maximum-gold/path-with-maximum-gold.java
@@ -1,29 +1,31 @@
 class Solution {
     boolean visited[][];
-    int maxGold;
-    public void helper(int[][] grid, int row, int col, int currGold){
-        if(row < 0 || col < 0 || row >= grid.length || col >= grid[0].length) return;
-        if(grid[row][col] == 0 || visited[row][col]) return ; 
+
+    public int helper(int[][] grid, int row, int col){
+        if(row < 0 || col < 0 || row >= grid.length || col >= grid[0].length) return 0;
+        if(grid[row][col] == 0 || visited[row][col]) return 0; 
+
         visited[row][col]= true;
-        currGold += grid[row][col];
+        
 
-        maxGold = Math.max(maxGold, currGold);
-
-        helper(grid, row + 1, col, currGold );
-        helper(grid, row - 1, col, currGold );
-        helper(grid, row, col + 1, currGold );
-        helper(grid, row, col - 1, currGold );
+        int down = helper(grid, row + 1, col);
+        int up = helper(grid, row - 1, col);
+        int right = helper(grid, row, col + 1);
+        int left = helper(grid, row, col - 1);
         
         visited[row][col] = false;
+        return  grid[row][col] + Math.max(Math.max(down, up), Math.max(right, left));
     }
     public int getMaximumGold(int[][] grid) {
         int n = grid.length;
         int m = grid[0].length;
+        int maxGold = 0;
         visited = new boolean[n][m];
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
                 if(grid[i][j] != 0){
-                    helper(grid, i, j, 0);
+                    maxGold = Math.max(maxGold, helper(grid, i, j));
+                    System.out.println(maxGold);
                 }
             }
         }
