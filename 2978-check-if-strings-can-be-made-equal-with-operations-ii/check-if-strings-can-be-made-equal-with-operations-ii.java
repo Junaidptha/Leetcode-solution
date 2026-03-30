b8e65ef7f431b0f6caa9cef3b467d26c2051dd54
Junaidptha <khan619junaid@gmail.com>
2026-03-30 12:12:26 +0530

    Time: 6 ms (61.84%) | Memory: 48 MB (53.95%) - LeetSync

--- 2978-check-if-strings-can-be-made-equal-with-operations-ii/check-if-strings-can-be-made-equal-with-operations-ii.java
+++ 2978-check-if-strings-can-be-made-equal-with-operations-ii/check-if-strings-can-be-made-equal-with-operations-ii.java
@@ -1,17 +1,17 @@
 class Solution {
     public boolean checkStrings(String s1, String s2) {
-        int[][] cnt = new int[2][26];
-        for (int i = 0; i < s1.length(); i++) {
-            int parity = i % 2;
-            cnt[parity][s1.charAt(i) - 'a']++;
-            cnt[parity][s2.charAt(i) - 'a']--;
-        }
+        int[][] freq = new int[2][26];
+		for (int i = 0; i < s1.length(); i++) {
+			freq[i % 2][s1.charAt(i) - 'a']++;
+			freq[i % 2][s2.charAt(i) - 'a']--;
+		}
+
+        for (int i = 0; i < 26; i++) {
+			if (freq[0][i] != 0 || freq[1][i] != 0) {
+				return false;
+			}
+		}
         
-        for (int p = 0; p < 2; p++) {
-            for (int c = 0; c < 26; c++) {
-                if (cnt[p][c] != 0) return false;
-            }
-        }
         return true;
 
     }
