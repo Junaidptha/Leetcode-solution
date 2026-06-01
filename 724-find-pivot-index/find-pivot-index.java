3d8b9487d8d4fe384b22c1d5ec0f76e3bc415f23
Junaidptha <khan619junaid@gmail.com>
2026-06-01 13:28:28 +0530

    Time: 315 ms (5.83%) | Memory: 47.5 MB (21.71%) - LeetSync

---
+++ 724-find-pivot-index/find-pivot-index.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        for(int i = 0; i < nums.length; i++){
+            int lsum = 0;
+            int rsum = 0;
+
+            for(int j = 0; j < i; j++){
+                lsum += nums[j];
+            }
+            for(int j = i + 1; j <= nums.length - 1; j++){
+                rsum += nums[j];
+            }
+            if(lsum == rsum) return i;
+        }
+        return -1;
+    }
+}
