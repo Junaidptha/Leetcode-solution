7bf3621b5ed9f85c6ebb47bece4720cb7006313e
Junaidptha <khan619junaid@gmail.com>
2026-09-06 20:49:24 +0530

    Time: 0 ms (100.00%) | Memory: 45.3 MB (28.99%) - LeetSync

---
+++ 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.java
@@ -0,0 +1,7 @@
+class Solution {
+    public boolean uniformArray(int[] nums1) {
+        // Always possible: if an odd number exists, use it to turn every even number odd;
+        // otherwise, all elements are already even.
+        return true; 
+    }
+}
